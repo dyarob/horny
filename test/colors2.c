84288eb43bdfdc319373297400f558fd308a9ca0
dyarob <tezzerethouston@gmail.com>
2022-02-07 14:59:27 +0100

    finished formatting or colors2 color palette

--- test/colors2.c
+++ test/colors2.c
@@ -1,13 +1,36 @@
 #include <ncurses.h>
 int	main(int ac, char **av){
+int	i;
 initscr();
 start_color();
 
-for(int i=0; i<256; i++){
-	if(i%24==0) printw("\n");
-	init_pair(i, i, 255-i);
-	attron(COLOR_PAIR(i));
-	printw("<3");}
+for(i=0; i<8; i++){
+init_pair(i, COLOR_BLACK, i);
+attron(COLOR_PAIR(i));
+printw(" %03d ", i);}
+
+printw("\n");
+
+for(; i<16; i++){
+init_pair(i, COLOR_BLACK, i);
+attron(COLOR_PAIR(i));
+printw(" %03d ", i);}
+
+printw("\n");
+
+for(; i<232; i++){
+if((i-16)%6==0) printw("\n");
+init_pair(i, COLOR_BLACK, i);
+attron(COLOR_PAIR(i));
+printw(" %03d ", i);}
+
+printw("\n");
+
+for(; i<256; i++){
+init_pair(i, COLOR_BLACK, i);
+attron(COLOR_PAIR(i));
+printw(" %03d ", i);
+if(!(i%8)) printw("\n");}
 
 getch();
 endwin();
