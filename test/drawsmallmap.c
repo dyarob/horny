5661a17ea3ba389af5861173585f07f6a0aef387
dyarob <tezzerethouston@gmail.com>
2022-02-08 09:46:51 +0100

    decent noise but not yet what I want

--- test/drawsmallmap.c
+++ test/drawsmallmap.c
@@ -3,8 +3,8 @@
 #include <time.h> //time()
 #include <ncurses.h>
 
-#define DSM_HEIGHT 5
-#define DSM_WIDTH 15
+#define DSM_HEIGHT 30
+#define DSM_WIDTH 90
 
 int	main(int ac, char **av){
 char	**m;
@@ -13,6 +13,8 @@ srand((unsigned)time(0));
 initscr(); start_color();
 init_pair(1, COLOR_BLACK, 213); //pink
 init_pair(2, COLOR_BLACK, 99); //purple
+init_pair(3, COLOR_BLACK, 177); //pink in-between
+init_pair(4, COLOR_BLACK, 135); //purple in-between
 
 //allocation
 m = (char **)malloc(sizeof(char *)*DSM_HEIGHT);
@@ -27,7 +29,45 @@ for(char i=0; i<DSM_HEIGHT; i++){ for(char j=0; j<DSM_WIDTH; j++){
 addch(m[i][j]+'0');} addch('\n');}
 
 for(char i=0; i<DSM_HEIGHT; i++){ for(char j=0; j<DSM_WIDTH; j++){
-attron(COLOR_PAIR((m[i][j]>=5?1:2))); addch(' ');} addch('\n');}
+if(m[i][j]==9)
+switch(i){
+case 0:
+	switch(j){
+	case 0:
+	if(m[i+1][j]!=9 && m[i][j+1]!=9) m[i][j]=8; break;
+	case DSM_WIDTH-1:
+	if(m[i+1][j]!=9 && m[i][j-1]!=9) m[i][j]=8; break;
+	default:
+	if(m[i+1][j]!=9 && m[i][j+1]!=9 && m[i][j-1]!=9) m[i][j]=8; break;}
+	break;
+case DSM_HEIGHT-1:
+	switch(j){
+	case 0:
+	if(m[i-1][j]!=9 && m[i][j+1]!=9) m[i][j]=8; break;
+	case DSM_WIDTH-1:
+	if(m[i-1][j]!=9 && m[i][j-1]!=9) m[i][j]=8; break;
+	default:
+	if(m[i-1][j]!=9 && m[i][j+1]!=9 && m[i][j-1]!=9) m[i][j]=8; break;}
+	break;
+default:
+	switch(j){
+	case 0:
+	if(m[i+1][j]!=9 && m[i-1][j]!=9 && m[i][j+1]!=9) m[i][j]=8; break;
+	case DSM_WIDTH-1:
+	if(m[i+1][j]!=9 && m[i-1][j]!=9 && m[i][j-1]!=9) m[i][j]=8; break;
+	default:
+	if(m[i+1][j]!=9 && m[i-1][j]!=9 && m[i][j+1]!=9 && m[i][j-1]!=9) m[i][j]=8; break;}
+	break;
+}
+//if(m[i][j]==9 && m[(i>0?i-1:0)][j]!=9 && m[(i==DSM_HEIGHT-1?DSM_HEIGHT-1:i+1)][j]!=9
+//	&& m[i][(j>0?j-1:0)]!=9 && m[i][(j==DSM_WIDTH-1?DSM_WIDTH-1:j+1)]!=9)
+//m[i][j] = 8;
+
+if(m[i][j]==9) attron(COLOR_PAIR(1));
+else if(m[i][j]==8) attron(COLOR_PAIR(3));
+else if(m[i][j]==7 || m[i][j]==6) attron(COLOR_PAIR(4));
+else attron(COLOR_PAIR(2));
+addch(' ');} addch('\n');}
 
 getch();
 endwin();
