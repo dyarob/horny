81f365bb358c9b56660d9a68b93244b909934d92
dyarob <tezzerethouston@gmail.com>
2022-02-09 18:03:02 +0100

    with some tweaks, but it sometimes crashes

--- test/drawsmallmap.c
+++ test/drawsmallmap.c
@@ -7,7 +7,7 @@
 #include "group.h"
 
 #define DSM_HEIGHT 30
-#define DSM_WIDTH 90
+#define DSM_WIDTH 45
 
 void	check_cell(char **m, char i, char j, group **g, char x);
 
@@ -120,6 +120,13 @@ default:
 //creating groups
 for(char i=0; i<DSM_HEIGHT; i++){ for(char j=0; j<DSM_WIDTH; j++){
 check_cell(m, i, j, &g, 0);}}
+
+//step 4b: remove smallest groups
+for(group *h=g; h->next; h=h->next){
+if(h->next->weight<=3){
+for(cell *c=h->next->list; c; c=c->next){ m[c->i][c->j]=0;}
+h->next=h->next->next;}}
+
 //calculating centers (rounded)
 for(group *h=g; h; h=h->next){ int i=0; int j=0;
 for(cell *c=h->list; c; c=c->next){ i+=c->i; j+=c->j;}
@@ -129,8 +136,7 @@ for(group *gg=g; gg; gg=gg->next){ group *hh=NULL; float f, ff=200;
 for(group *gh=g; gh; gh=gh->next){
 if(gh!=gg){
 f=sqrt((gg->c.i-gh->c.i)*(gg->c.i-gh->c.i)+(gg->c.j-gh->c.j)*(gg->c.j-gh->c.j));
-if(f<ff){ ff=f; hh=gh;}}
-//printw("ff= %f  ", ff);
+if(f<ff){ ff=f; hh=gh;}}//debug: printw("ff= %f  ", ff);
 }
 //calculating increments
 float inc, jnc;
@@ -138,15 +144,12 @@ if(!(hh->c.j-gg->c.j)) if(hh->c.i-gg->c.i<0) inc=-1; else inc=1;
 else inc=(float)(hh->c.i-gg->c.i)/abs(hh->c.j-gg->c.j);
 if(!(hh->c.i-gg->c.i)) if(hh->c.j-gg->c.j<0) jnc=-1; else jnc=1;
 else jnc=(float)(hh->c.j-gg->c.j)/abs(hh->c.i-gg->c.i);
-//printw("inc= %f and jnc= %f.  ", inc, jnc);
+//debug: printw("inc= %f and jnc= %f.  ", inc, jnc);
 if(inc>1) inc=1; else if(inc<-1) inc=-1;
 if(jnc>1) jnc=1; else if(jnc<-1) jnc=-1;
-
+//drawing connections
 for(char i=1; hh->c.i-gg->c.i-i*inc>0 || hh->c.j-gg->c.j-i*jnc>0; i++){
-m[abs((int)(gg->c.i+i*inc))][abs((int)(gg->c.j+i*jnc))]=9;
-}
-
-}
+m[abs((int)(gg->c.i+i*inc))][abs((int)(gg->c.j+i*jnc))]=8;}}
 
 //step 5: add 7 around 8
 for(char i=0; i<DSM_HEIGHT; i++){ for(char j=0; j<DSM_WIDTH; j++){
@@ -181,23 +184,22 @@ default:
 
 //display
 for(char i=0; i<DSM_HEIGHT; i++){ for(char j=0; j<DSM_WIDTH; j++){
-addch(m[i][j]+'0');} addch('\n');}
+addch(m[i][j]+'0'); addch(' ');} addch('\n');}
 
 for(char i=0; i<DSM_HEIGHT; i++){ for(char j=0; j<DSM_WIDTH; j++){
 if(m[i][j]==9) attron(COLOR_PAIR(1));
 else if(m[i][j]==8) attron(COLOR_PAIR(3));
 else if(m[i][j]==7 || m[i][j]==6) attron(COLOR_PAIR(4));
 else attron(COLOR_PAIR(2));
-addch(' ');} addch('\n');}
+addch(' '); addch(' ');} addch('\n');}
 
 printw("\ngroup weights:\n");
 for(group *h=g; h; h=h->next){
 printw("%d ", h->weight);}
 
-/*//print group center (debug)
+//print group center (debug)
 for(group *h=g; h; h=h->next){
-mvaddch(DSM_HEIGHT+h->c.i, h->c.j, 'o');}
-*/
+mvaddch(DSM_HEIGHT+h->c.i, h->c.j*2, 'o');}
 
 getch();
 endwin();
